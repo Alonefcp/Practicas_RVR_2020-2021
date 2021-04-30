b1c445d419625ae92f4b36421d03a223bdec4660
Alonefcp <fcuadra@ucm.es>
2021-04-30 21:56:58 +0200

    Practica2.1: Ejercicio3 y cambios en el ejercicio2

--- Practica2.1/Ejercicio2.cc
+++ Practica2.1/Ejercicio2.cc
@@ -1,22 +1,15 @@
 #include <sys/types.h>
-
 #include <sys/socket.h>
-
 #include <netdb.h>
-
 #include <unistd.h>
 
-
-
 #include <iostream>
-
 #include <string.h>
 
-
+#define TAMBUFFER 80
 
 int main(int argc ,char *argv[])
 {
-
     struct addrinfo hints;
     struct addrinfo *res;
 
@@ -24,6 +17,7 @@ int main(int argc ,char *argv[])
 
     hints.ai_family   = AF_INET; 
     hints.ai_socktype = SOCK_DGRAM;
+
 
     int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
 
@@ -33,8 +27,8 @@ int main(int argc ,char *argv[])
         return -1;
     }  
 
+    
     int sd = socket(res->ai_family, res->ai_socktype, 0);
-
     if(sd==-1)
     {
         std::cerr<<"[socket] creacion de socket"<<std::endl;
@@ -46,27 +40,24 @@ int main(int argc ,char *argv[])
     freeaddrinfo(res);
 
     while(true)
-
     {
-        char buffer[80];
+        char buffer[TAMBUFFER];
         char host[NI_MAXHOST];
         char serv[NI_MAXSERV];
 
         struct sockaddr cliente;
         socklen_t cliente_len = sizeof(struct sockaddr);
-
-        int bytes = recvfrom(sd, (void*) buffer, 79, 0, &cliente, &cliente_len);
+        
+        int bytes = recvfrom(sd, (void*) buffer, TAMBUFFER-1, 0, &cliente, &cliente_len);
         buffer[bytes]= '\0';
 
         if(bytes==-1)
         {
-            std::cerr<<"Error recieving messages"<<std::endl;
-
+            std::cerr<<"[recvfrom]:Error recieving messages"<<std::endl;
             return -1;
         }
 
         getnameinfo(&cliente, cliente_len, host, NI_MAXHOST,serv, NI_MAXSERV, NI_NUMERICHOST|NI_NUMERICSERV);
-
         std::cout << "Host: " << host << ", Port: " << serv << std::endl; 
 
         time_t rawtime;
@@ -75,39 +66,29 @@ int main(int argc ,char *argv[])
         timeinfo = localtime (&rawtime);
 
         if(std::tolower(buffer[0])=='t')
-
         {
-
-            int tam = strftime(buffer,79,"%T",timeinfo);
-
+            int tam = strftime(buffer,TAMBUFFER-1,"%T",timeinfo) + 1;
             sendto(sd, buffer, tam, 0, &cliente, cliente_len);
-
         } 
         else if(std::tolower(buffer[0])=='d')
         {
-
-            int tam = strftime(buffer,79,"%D",timeinfo);
-
+            int tam = strftime(buffer,TAMBUFFER-1,"%D",timeinfo) + 1;
             sendto(sd, buffer, tam, 0, &cliente, cliente_len);
         }
         else if(buffer[0]=='q')
         {
-
             std::cout<< "Saliendo... " << std::endl;
-
             break;
         } 
         else
         {
-
             std::cout<< "Comando no soportado: " << buffer[0] << std::endl;
             std::cout<< "Comandos soportados: t, d, q" << std::endl;
-
-        }          
-
+        }
+           
     }
 
     close(sd);
-    return 0;
 
+    return 0;
 }
---
+++ Practica2.1/Ejercicio3.cc
@@ -0,0 +1,60 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <string.h>
+
+#define TAMBUFFER 80
+
+int main(int argc ,char *argv[])
+{
+    struct addrinfo hints;
+    struct addrinfo *res;
+
+    memset((void*) &hints, 0, sizeof(struct addrinfo));
+
+    hints.ai_family   = AF_INET; 
+    hints.ai_socktype = SOCK_DGRAM;
+
+    int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
+    if(rc!=0)
+    {
+        std::cerr << "[getaddrinfo]: " << gai_strerror(rc) << std::endl;
+        return -1;
+    }    
+
+    int sd = socket(res->ai_family, res->ai_socktype, 0);
+    if(sd==-1)
+    {
+        std::cerr<<"[socket] creacion de socket"<<std::endl;
+        return -1;
+    }
+
+    freeaddrinfo(res);
+  
+    int s = sendto(sd, argv[3], strlen(argv[3])+1, 0, res->ai_addr, res->ai_addrlen);
+    if(s==-1)
+    {
+        std::cout<<"[sendto]: Error enviando mensajes\n";
+        return -1;
+    }
+    
+    
+    char buffer[TAMBUFFER];
+    int bytes = recvfrom(sd, (void*) buffer, TAMBUFFER, 0, res->ai_addr, &res->ai_addrlen);
+    buffer[bytes]= '\0';
+
+    if(bytes==-1)
+    {
+        std::cerr<<"[recvfrom]: Error recibiendo mensajes"<<std::endl;
+        return -1;
+    }
+     
+    std::cout << buffer << std::endl;
+
+    close(sd);
+
+    return 0;
+}
